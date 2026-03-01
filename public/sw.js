00de68384c4da76c8151e1216c07ddcdcbb8d2c9
Roberto Moreno <rampmaster@gmail.com>
2026-03-01 11:40:57 -0300

    fear(newmodel): Add info about new bussiness model

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+// public/sw.js
+const CACHE_NAME = 'rampmaster-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo_black.png',
+  '/logo_white.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
